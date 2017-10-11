bd55102875afa1d0681aea9d246574c75517fed3
fornesarturo <fornesarturo@gmail.com>
2017-10-11 15:05:19 -0500

    Trying to recognize

--- app/src/main/java/kr/has/perro/opencvkorean/MainActivity.java
+++ app/src/main/java/kr/has/perro/opencvkorean/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -17,14 +18,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
 
 import org.opencv.android.OpenCVLoader;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -43,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
     private static final int TAKE_PICTURE = 1;
     private static final int STORAGE_PERMISSION = 2;
     private String lastImageURI;
+    private static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/OpenCVKorean/";
+    private static final String lang = "eng";
 
     TextView text;
     ImageView image;
@@ -55,6 +62,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         text = (TextView) findViewById(R.id.tv_image_text);
         image = (ImageView) findViewById(R.id.iv_image);
+
+
+        String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+
+        for (String path : paths) {
+            File dir = new File(path);
+            if (!dir.exists()) {
+                if (!dir.mkdirs()) {
+                    Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+                    return;
+                } else {
+                    Log.v(TAG, "Created directory " + path + " on sdcard");
+                }
+            }
+
+        }
+
+        // lang.traineddata file with the app (in assets folder)
+        // You can get them at:
+        // http://code.google.com/p/tesseract-ocr/downloads/list
+        // This area needs work and optimization
+        if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+            try {
+
+                AssetManager assetManager = getAssets();
+                InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
+                //GZIPInputStream gin = new GZIPInputStream(in);
+                OutputStream out = new FileOutputStream(DATA_PATH
+                        + "tessdata/" + lang + ".traineddata");
+
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                //while ((lenf = gin.read(buff)) > 0) {
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                //gin.close();
+                out.close();
+
+                Log.v(TAG, "Copied " + lang + " traineddata");
+            } catch (IOException e) {
+                Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+            }
+        }
     }
 
     @Override
@@ -66,9 +119,43 @@ public class MainActivity extends AppCompatActivity {
                     Log.d(TAG, "Image URI: " + lastImageURI);
                     imageBitmap = BitmapFactory.decodeFile(lastImageURI);
                     image.setImageBitmap(imageBitmap);
+                    getTextFromImage();
                     break;
             }
         }
+    }
+
+    private void getTextFromImage() {
+        Log.d(TAG, "Starting OCR");
+        Bitmap bitmap = imageBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Log.d(TAG, "Copy bitmap");
+        TessBaseAPI baseAPI = new TessBaseAPI();
+        baseAPI.setDebug(true);
+        baseAPI.init(DATA_PATH, lang);
+        Log.d(TAG, "Initialized BaseAPI");
+        baseAPI.setImage(bitmap);
+        Log.d(TAG, "Set Image");
+        String recognizedText = "";
+        try {
+            //recognizedText = baseAPI.getUTF8Text();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        baseAPI.end();
+
+        Log.v(TAG, "OCRED TEXT: " + recognizedText);
+
+        if ( lang.equalsIgnoreCase("eng") ) {
+            recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
+        }
+
+        recognizedText = recognizedText.trim();
+
+        if ( recognizedText.length() != 0 ) {
+            text.setText(recognizedText);
+        }
+
     }
 
     private File createImageFile() throws IOException {
